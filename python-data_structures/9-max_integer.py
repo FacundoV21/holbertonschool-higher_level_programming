87abdd78521bee136e4e9095f34d243d38d42284
FacundoV21 <6756@holbertonstudents.com>
2023-09-24 22:41:35 +0000

    fixed task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     num = my_list[0]
-    for i in range(1, len(my_list) - 1):
+    for i in range(1, len(my_list)):
         if my_list[i] > num:
             num = my_list[i]
     return num
