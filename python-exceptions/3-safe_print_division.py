5783c4129e092ce639535a8728469646e3f71e23
FacundoV21 <6756@holbertonstudents.com>
2023-09-29 13:07:06 +0000

    added tasks 3 and 4

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        n = a / b
+    except Exception:
+        n = None
+    finally:
+        print("Inside result: {}".format(n))
+        return n
