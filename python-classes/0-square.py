0bad9f5b6ad1a1d44e20240c48a2dc0652fe1cb0
FacundoV21 <6756@holbertonstudents.com>
2023-09-30 16:00:45 +0000

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This file contains a class representing a square with all it's atributes
+    and methods.
+"""
+
+
+class Square:
+
+    """This class describes a square as well as all it's properties."""
+
+    pass
