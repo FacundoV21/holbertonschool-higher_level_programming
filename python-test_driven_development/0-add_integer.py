0778f9e65cecf02c2f6ae133985e057b3d33f430
FacundoV21 <6756@holbertonstudents.com>
2023-10-01 19:54:33 +0000

    fixed pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,8 @@
     Add function, which adds two integers, the tests for this function are
     located on the tests folder
 """
+
+
 def add_integer(a, b=98):
     """
         Returns the addition of two numbers if they are ints or floats
@@ -11,10 +13,10 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
-    
+
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    
+
     return a + b
