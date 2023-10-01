6d37c5f27815ed102263e1735d22a02cba5f8c9d
FacundoV21 <6756@holbertonstudents.com>
2023-10-01 19:51:34 +0000

    added task0 and readme.md

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Add function, which adds two integers, the tests for this function are
+    located on the tests folder
+"""
+def add_integer(a, b=98):
+    """
+        Returns the addition of two numbers if they are ints or floats
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    
+    return a + b
