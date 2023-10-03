7c891eff9d5361246b2590665b84f1c11b23b623
FacundoV21 <6756@holbebertonstudents.com>
2023-10-03 14:48:55 +0000

    task 0 and 1 added

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This file contains a class representing a Rectangle with all it's atributes
+    and methods.
+"""
+
+
+class Rectangle:
+
+    """This class describes a Rectangle as well as all it's properties."""
+    pass
