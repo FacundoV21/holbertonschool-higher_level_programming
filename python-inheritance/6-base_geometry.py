0e90d9946d71fd500e939ea3da903af2238bb609
FacuV21 <6756@Holbertonstudents.com>
2023-10-06 13:31:52 +0000

    added task 5 and 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    add area imp to class
+"""
+
+
+class BaseGeometry():
+    """
+        area added
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
