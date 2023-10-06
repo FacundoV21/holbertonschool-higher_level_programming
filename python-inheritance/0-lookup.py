144c597d0cbb58c5646c54cd71ca91d5a425f061
FacuV21 <6756@Holbertonstudents.com>
2023-10-06 11:07:02 +0000

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+    Module to declare list of available attributes and methods of an object
+"""
 def lookup(obj):
+    """
+        Returns the list
+    """
     return dir(obj)
