607f47f64f70fbb6e95680a5848667906f6c919b
FacuV21 <6756@Holbertonstudents.com>
2023-10-06 11:48:00 +0000

    task 2 added

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module to declare list of available attributes and methods of an object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        returns true or false depending if an obj is instance of a_class
+    """
+    return isinstance(obj, a_class)
