deda6c115b549ba3bac91ae7c9cbbed549b01fed
FacundoV21 <6756@holbertonstudents.com>
2023-10-07 03:32:56 +0000

    added task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Defines a square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square class
+    """
+    def __init__(self, size):
+        """
+            Class wich inherits Rectangle
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+            returns the area of square
+        """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """
+            Return string
+        """
+        return (f"[Square] {self.__size}/{self.__size}")
