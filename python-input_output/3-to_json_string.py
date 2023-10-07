e522ab5399a5e5b6327211f2b9b8c3b855275cd8
FacundoV21 <6756@holbertonstudents.com>
2023-10-07 22:17:51 +0000

    added task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Start of the program
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+        Returns JSON representation of an object
+    """
+    return json.dumps(my_obj)
