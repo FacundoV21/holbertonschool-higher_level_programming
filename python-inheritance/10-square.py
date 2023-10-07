137d29c24f83bc4c04467175826a38935abd2f98
FacundoV21 <6756@holbertonstudents.com>
2023-10-07 03:23:20 +0000

    fixed task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """
-            Class wich inherits from Rectangle
+            Class wich inherits Rectangle
         """
         super().integer_validator("size", size)
         super().__init__(size, size)
