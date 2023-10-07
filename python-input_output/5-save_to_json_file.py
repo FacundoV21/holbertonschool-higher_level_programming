ec7811c9673d09654e09acf9806d27c6cb87d1c1
FacundoV21 <6756@holbertonstudents.com>
2023-10-07 22:44:57 +0000

    added tasks 4 and

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Start of the program
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        Returns an object represented by a JSON string
+    """
+    copy = json.dumps(my_obj)
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        num = f.write(copy)
