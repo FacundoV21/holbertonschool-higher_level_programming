9f82d9a951ad04f415796b021dc5ce3251141a01
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 13:07:08 +0000

    fixed 2 files

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,11 +10,11 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 if exists("add_item.json"):
-    cont = load_from_json_file("add_item.json")
+    contenido = load_from_json_file("add_item.json")
 else:
-    cont = []
+    contenido = []
 
 for i in range(1, len(argv)):
-    cont.append(argv[i])
+    contenido.append(argv[i])
 
-save_to_json_file(cont, "add_item.json")
+save_to_json_file(contenido, "add_item.json")
