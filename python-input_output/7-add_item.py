a1887913c464d3cf24e2b1584b4de271fc740a02
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 02:25:44 +0000

    fixed task 7 again

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@
 from sys import argv
 from os.path import exists
 import json
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 if exists("add_item.json"):
