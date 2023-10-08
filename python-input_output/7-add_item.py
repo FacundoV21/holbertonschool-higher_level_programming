1c960b7343467641226d6c478a2bb9154eaf9d0c
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 01:33:44 +0000

    fixed task 7 againn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,16 +11,15 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    """
-        Main Function, adds all arguments to a Python list, and then saves
-        them to a file
-    """
+"""
+    Main Function, adds all arguments to a Python list, and then saves
+    them to a file
+"""
 
-    fileName = "add_item.json"
-    cont = load(fileName)
+fileName = "add_item.json"
+cont = load(fileName)
 
-    with open(fileName, "w", encoding="utf-8") as f:
-        for i in range(1, len(argv)):
-            cont.append(argv[i])
-        save(cont, fileName)
+with open(fileName, "w", encoding="utf-8") as f:
+    for i in range(1, len(argv)):
+        cont.append(argv[i])
+    save(cont, fileName)
