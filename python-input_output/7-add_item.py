f311e1aca430354b77bb8971ee8eb0cf87a8713b
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 01:15:29 +0000

    fixed task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,6 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 
-
 """
     Main Function, adds all arguments to a Python list, and then saves
     them to a file
