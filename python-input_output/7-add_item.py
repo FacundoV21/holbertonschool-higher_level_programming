bc768b448482b6a42fa2a7f884d50392467ccb4a
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 01:49:53 +0000

    fixed task 7 again

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,15 +5,16 @@
 from sys import argv
 from os.path import exists
 import json
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
 if exists("add_item.json"):
-    cont = load("add_item.json")
+    cont = load_from_json_file("add_item.json")
 else:
     cont = []
 
 for i in range(1, len(argv)):
     cont.append(argv[i])
-save(cont, "add_item.json")
+
+save_to_json_file(cont, "add_item.json")
