02c50ed6133f20580124b0921de09f6bb4b3b9bb
FacundoV21 <6756@holbertonstudents.com>
2023-10-08 01:07:55 +0000

    changed task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
         Returns an object represented by a JSON string
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
