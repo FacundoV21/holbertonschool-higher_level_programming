5361ce114d431d09fb426efe708a712c820a48ab
FacundoV21 <6756@holbertonstudents.com>
2023-11-16 19:13:07 -0800

    fixed task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- Creates table if does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE if not exists states(id INT AUTO INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE if not exists states(id INT UNIQUE AUTO INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
