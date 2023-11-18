2079350705f29f7c928c51f93d5cdc9ea8479e76
FacundoV21 <6756@holbertonstudents.com>
2023-11-18 04:35:33 -0800

    fixed task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,4 @@
 -- first join
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.sate_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name 
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
