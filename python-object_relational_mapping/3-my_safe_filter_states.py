0f3d34437b5712f8c19ab4f76e40cf090cb0dad9
FacundoV21 <6756@holbertonstudents.com>
2023-11-19 11:55:11 -0800

    added task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Selects all states from a database
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+
+    con = database.cursor()
+    con.execute(
+        f"""
+            SELECT *
+            FROM states
+            WHERE name = %s
+            ORDER BY states.id ASC;
+        """, (argv[4],)
+    )
+    data = con.fetchall()
+    for i in data:
+        print(i)
+
+con.close()
+database.close()
