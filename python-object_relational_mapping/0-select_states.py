0267deafd4abc6454839edeec20409a13e866947
FacundoV21 <6756@holbertonstudents.com>
2023-11-19 11:34:55 -0800

    fixed tasks 0 to 2

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,20 +6,21 @@
 from sys import argv
 import MySQLdb
 
-database = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    database=argv[3]
-)
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
 
-con = database.cursor()
-con.execute(
-    """SELECT *
-        FROM states
-        ORDER BY id ASC;
-    """
-)
-data = con.fetchall()
-for i in data:
-    print(i)
+    con = database.cursor()
+    con.execute(
+        """SELECT *
+            FROM states
+            ORDER BY id ASC;
+        """
+    )
+    data = con.fetchall()
+    for i in data:
+        print(i)
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,21 +6,22 @@
 from sys import argv
 import MySQLdb
 
-database = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    database=argv[3]
-)
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
 
-con = database.cursor()
-con.execute(
-    """SELECT *
-        FROM states
-        WHERE name LIKE 'N%'
-        ORDER BY id ASC;
-    """
-)
-data = con.fetchall()
-for i in data:
-    print(i)
+    con = database.cursor()
+    con.execute(
+        """SELECT *
+            FROM states
+            WHERE name LIKE 'N%'
+            ORDER BY id ASC;
+        """
+    )
+    data = con.fetchall()
+    for i in data:
+        print(i)
