c54b7de1e4f1f1d8907b93646efe9f07b750910c
FacundoV21 <6756@holbertonstudents.com>
2024-02-03 21:08:47 +0000

    added task 0 to 3

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,9 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const headerElement = document.querySelector('#header');
-
-  if (headerElement) {
-    headerElement.style.color = '#FF0000';
-  } else {
-    console.error('Header element not found.');
-  }
-});
+const headerElement = document.querySelector(' header');
+headerElement.style.color = '#FF0000';
